f249de3dcc0f4dc1c01a0d3807bd5ec794bd9124
Emmanuel Lafenwa <103007301+leoemaxie@users.noreply.github.com>
2023-07-30 13:07:28 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 # Script to generate libhash static library for testing.
 set -u
 
-gcc -c *.c -g -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+gcc -c *.c -g -Wall -Werror -Wextra -pedantic -std=gnu89
 ar -rcs libhash.a *.o
 gcc *.o -o hash
 
